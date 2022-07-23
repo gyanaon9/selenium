e090e657f616db2762ec428e3020b26a042390df
DELL <DELL@DESKTOP-DQ6BLN8>
2022-07-23 14:13:40 -0700

    update

--- src/DDT/CrossBrowser.java
+++ src/DDT/CrossBrowser.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class CrossBrowser {
+public class CrossBrowser {//...............................
 //how to data parameter through testng.xml file to a test case?
 	WebDriver driver;
 	@Parameters("browser")
